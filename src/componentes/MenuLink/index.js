d6228b5e8535d2416ae8b83dd976ed08dc9c6f90
birajnior <ubiratanbernardo@gmail.com>
2024-01-10 22:45:18 -0300

    Rotas de navegação

---
+++ src/componentes/MenuLink/index.js
@@ -0,0 +1,18 @@
+import { Link, NavLink, useLocation } from "react-router-dom";
+import styles from "./MenuLink.module.css";
+
+export default function MenuLink({ children, to}) {
+  const localizacao = useLocation();
+  console.log(localizacao);
+  return (
+    <Link
+      className={`
+          ${styles.link} 
+          ${localizacao.pathname === to ? styles.linkDestacado : ""}`}
+      to={to}
+    >
+      {children}
+    </Link>
+    
+  );
+}
