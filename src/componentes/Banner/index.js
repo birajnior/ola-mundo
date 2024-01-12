af6063808bfb939f702fce6da06a1ac6b50312c9
birajnior <ubiratanbernardo@gmail.com>
2024-01-12 16:45:09 -0300

    finalizando banner

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -1,5 +1,30 @@
 import styles from "./Banner.module.css";
+import circuloColorido from "assets/circulo_colorido.png";
+import minhaFoto from "assets/minha_fotoo.png";
 
 export default function Banner() {
-  return "Banner"
+  return (
+    <div className={styles.banner}>
+      <div className={styles.apresentacao}>
+        <h1 className={styles.titulo}>Olá, Mundo!</h1>
+        <p className={styles.paragrafo}>
+          Bem-vindo ao meu portfólio! Aqui, você encontrará meu mundo de
+          desenvolvimento front-end, onde transformo ideias em experiências
+          digitais cativantes. Cada projeto reflete minha paixão por criar
+          interfaces intuitivas e visualmente envolventes. Explore e inspire-se
+          nesta jornada digital!
+        </p>
+      </div>
+      <div className={styles.imagens}>
+        <img
+          className={styles.circuloColorido}
+          src={circuloColorido}
+          alt="Circulo Colorido"
+          aria-hidden={true}
+        />
+
+        <img className={styles.minhaFoto} src={minhaFoto} alt="Minha Foto" />
+      </div>
+    </div>
+  );
 }
--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,11 @@
+import Banner from "componentes/Banner";
+
 export default function SobreMim() {
   return (
+    <main>
+    <Banner />
+
     <h1>Sobre mim!</h1>
+  </main>
   )
 }
